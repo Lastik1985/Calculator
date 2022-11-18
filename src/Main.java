eb9419e2ac73d389827a53f8615dad2f65601395
Lastik1985 <Lastik1985@mail.ru>
2022-11-18 12:42:14 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 public class Main {
-    public Main() {
-    }
-
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
 
         int c;
-        try {
-            c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (ArithmeticException var6) {
-            System.out.println("Делить на ноль нельзя!");
-        }
+
 
         c = calc.devide.apply(100, 20);
         calc.println.accept(c);
